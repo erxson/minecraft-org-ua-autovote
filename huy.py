ca5d23413dcf8f6d38803a1a4aa8f4bd714ac85c
Amir <50082485+erxson@users.noreply.github.com>
2023-06-05 03:34:44 +0300

    Update huy.py

--- huy.py
+++ huy.py
@@ -1,55 +1,44 @@
+import sys
+import concurrent.futures
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import concurrent.futures
-import signal
+from tqdm import tqdm
 
-# Чтение списка прокси из файла
 with open('proxies.txt', 'r') as file:
     proxies = file.read().splitlines()
 
-# Функция для проверки прокси
 def check_proxy(proxy):
     try:
-        # Настройка Chrome Selenium WebDriver
         chrome_options = Options()
-        chrome_options.add_argument('--headless')  # Включение скрытого режима
+        chrome_options.add_argument('--headless')
         chrome_options.add_argument(f'--proxy-server=socks4://{proxy}')
-        
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        chrome_options.add_argument('--disable-dev-shm-usage')
+
         with webdriver.Chrome(options=chrome_options) as driver:
-            # Загрузка страницы
             driver.get('https://minecraft.org.ua/minecraft-servers/Helix-Vanilla/536')
 
-            # Ожидание появления кнопки
             button_locator = (By.CSS_SELECTOR, 'button.mc-button:nth-child(2)')
             WebDriverWait(driver, 10).until(EC.presence_of_element_located(button_locator))
 
-            # Нажатие на кнопку
             button = driver.find_element(*button_locator)
             button.click()
-            print(f"Нажал на прокси {proxy}")
+
+            return proxy
 
     except Exception as e:
-        print(f'Произошла ошибка с прокси {proxy}')
+        return proxy
 
-# Функция для обработки сигнала прерывания (Ctrl+C)
-def handle_interrupt(signal, frame):
-    print('Прерывание скрипта...')
-    # Остановка выполнения потоков и завершение работы браузера
-    executor.shutdown(wait=False)
-    for thread in threads:
-        thread.join()
-    # Выход из программы
-    sys.exit(0)
 
-# Регистрация обработчика сигнала прерывания
-signal.signal(signal.SIGINT, handle_interrupt)
+results = []
 
-# Использование пула потоков
-with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
-    # Сохранение ссылки на пул потоков для последующего завершения
-    threads = executor.map(check_proxy, proxies)
-    # Ожидание завершения всех потоков
-    executor.shutdown(wait=True)
+with tqdm(total=len(proxies)) as pbar:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=25) as executor:
+        threads = executor.map(check_proxy, proxies)
+        
+        for result in threads:
+            pbar.update(1)
